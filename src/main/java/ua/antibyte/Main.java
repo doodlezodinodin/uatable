118e27091102a3da8b401243eaf95d7e53feed20
Aleksandr <doodlezodinodin@gmail.com>
2019-10-20 16:47:44 +0300

    Add helper "PageGenerator" Add new servlet "IndexServlet" Run server

--- src/main/java/ua/antibyte/Main.java
+++ src/main/java/ua/antibyte/Main.java
@@ -1,7 +1,25 @@
 package ua.antibyte;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import ua.antibyte.servlets.IndexServlet;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws Exception {
+        System.out.println("Run");
+
+        IndexServlet indexServlet = new IndexServlet();
+
+        Server server = new Server(8080);
+
+        ServletContextHandler servletContextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        servletContextHandler.addServlet(new ServletHolder(indexServlet), "/authform");
+
+        server.setHandler(servletContextHandler);
+
+        server.start();
+        server.join();
+        System.out.println("test join");
     }
 }
